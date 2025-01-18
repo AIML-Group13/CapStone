571a8d3735c12cf35d1b023d7c136294af5304af
AIML-Group13 <aimlgroup13.iiith@gmail.com>
2025-01-18 16:43:00 +0530

    Updating the location to YoloV5 and best.pt file

--- yolo_module.py
+++ yolo_module.py
@@ -12,8 +12,8 @@ def vehicle_count(csv_path, input_type):
     return df
 
 def ambulance_detection(img_path):
-    !python /content/yolov5/detect.py --weights /content/drive/MyDrive/Results/ER/weights/best.pt --img 640 --conf 0.4 --save-csv --source "{img_path}"
-    latest_exp = sorted(Path('/content/yolov5/runs/detect').glob('exp*'), key=os.path.getmtime)[-1]
+    !python ../yolov5/detect.py --weights ../best.pt --img 640 --conf 0.4 --save-csv --source "{img_path}"
+    latest_exp = sorted(Path('../yolov5/runs/detect').glob('exp*'), key=os.path.getmtime)[-1]
     filenames = [f for f in os.listdir(latest_exp) if f.endswith(('.jpeg', '.jpg'))][0]
     print(filenames)
     return f"{latest_exp}/{filenames}"
